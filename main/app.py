8c8776068337793bb22e3081f45eb2ed8a278a6d
Nicolas Sirghii <nicolas.mailbox100@gmail.com>
2025-12-28 09:21:38 +0200

    forth

--- main/app.py
+++ main/app.py
@@ -6,10 +6,7 @@ app = FastAPI()
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://easyretain.com",
-        "https://www.easyretain.com"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
